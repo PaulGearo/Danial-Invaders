0e2e6e73829acdb632d7d63c77cf765635f68e50
Paul Gearo <PaulGearo@gmail.com>
2019-02-19 15:39:46 -0600

    fixed sizes, added comments

--- mainSpaceInvadors.py
+++ mainSpaceInvadors.py
@@ -1,17 +1,28 @@
 import pygame
 
-windowWidth = 900
-windowHeight = 1000
+windowWidth = 400
+windowHeight = 600
+game_side_margin = 10
+game_top_margin = 40
+game_bottom_margin = game_top_margin
+game_border_width = 3
+
+wall_top = game_top_margin + game_border_width
+wall_left = game_side_margin + game_border_width
+wall_right = windowWidth - game_side_margin - game_border_width
+wall_bottom = windowHeight - game_bottom_margin - game_border_width
 
 black = (0,0,0)
 white = (255,255,255)
-blue = (40,80,255)
+blue = (0,0,255)
 
 pygame.init()
 
-playerImg = pygame.image.load("daniel-headshot.png")
-enemyImg = pygame.image.load("definitely-not-daniel.png")
+# load game images
+playerImg = pygame.image.load("smol_daniel_emoji.jpg")
+enemyImg = pygame.image.load("smol_DefinitelyNotDaniel.jpg")
 bulletImg = pygame.image.load("si-bullet.gif")
+backroundImg = pygame.image.load("si-background.gif")
 
 gameDisplay = pygame.display.set_mode((windowWidth,windowHeight))
 pygame.display.set_caption('Danial Invaders')
@@ -26,6 +37,11 @@ class GameObject(object):
         self.height = image.get_height()
     def show(self):
         gameDisplay.blit(self.img, (self.xcor, self.ycor))
+    def collides_with(self, foreign_object):
+        return foreign_object.xcor < self.xcor + self.width \
+        and foreign_object.xcor + foreign_object.width > self.xcor \
+        and foreign_object.ycor < self.ycor + self.height \
+        and foreign_object.ycor + self.height > self.ycor
 
 class Player(GameObject):
     def __init__(self, xcor, ycor, image, speed):
@@ -34,7 +50,7 @@ class Player(GameObject):
         self.direction = 0
     def show(self):
         new_xcor = self.xcor + self.direction * self.speed
-        if new_xcor < 0 or new_xcor > windowWidth - self.width:
+        if new_xcor < wall_left or new_xcor > wall_right - self.width:
             self.xcor = self.xcor
         else:
             self.xcor = new_xcor
@@ -67,22 +83,25 @@ class Bullet(GameObject):
     def move_up(self):
         self.ycor -= self.speed
 
-
 clock = pygame.time.Clock()
 
-
-
-player1 = Player(200, 870, playerImg, 5)
+player1 = Player(wall_left + (wall_right - wall_left) / 2 - playerImg.get_width() / 2,
+    wall_bottom - playerImg.get_height() -1, playerImg, 5)
 
 enemies = []
 bullets = []
 
-for x in range(0,5):
-    newEnemy = Enemy((enemyImg.get_width() + 5) * x + 1, 10, enemyImg, 2)
-    enemies.append(newEnemy)
+for row in range (0, 3):
+    for column in range(0,5):
+        newEnemy = Enemy((enemyImg.get_width() + 5) * column + wall_left + 1,\
+            (enemyImg.get_height( ) + 5) * row + wall_top + 1,\
+         enemyImg, 2)
+        enemies.append(newEnemy)
 
+# main game loop
 while player1.is_alive:
 
+    # event handling
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             player1.is_alive = False
@@ -97,10 +116,27 @@ while player1.is_alive:
 
     gameDisplay.blit(gameDisplay, (0,0))
     gameDisplay.fill(black)
+    pygame.draw.rect(gameDisplay, white, (game_side_margin, game_top_margin, windowWidth - game_side_margin * 2, windowHeight - game_top_margin - game_bottom_margin))
+    gameDisplay.blit(backroundImg, (wall_left, wall_top), (0, 0, wall_right - wall_left, wall_bottom - wall_top))
+
+    # check each bullet to see if it hits an enemy
+    for bullet in bullets:
+        # check if this bullet has gone off the top
+        if bullet.ycor < wall_top:
+            bullets.remove(bullet)  
+            continue
+        # check if this bullet has hit any of the enemies
+        for enemy in enemies:
+            # if the bullet has collided with an enemy, romove both from their arrays
+            if bullet.collides_with(enemy):
+                enemies.remove(enemy)
+                bullets.remove(bullet)
+                break
+           
 
     # check all enimies to see if one has rached a wall
     for enemy in enemies:
-        if enemy.xcor <= 0 or enemy.xcor >= windowWidth - enemy.width:
+        if enemy.xcor <= wall_left or enemy.xcor >= wall_right - enemy.width:
             #since one enemy has reached a wall, change all enimies directions
             for e in enemies:
                 e.change_direction()
@@ -113,23 +149,14 @@ while player1.is_alive:
         enemy.move_over()
         enemy.show()
 
+     # move and show all bullets
     for bullet in bullets:
         bullet.move_up()
         bullet.show()
 
     player1.show()
-
- 
+    
     pygame.display.update()
-
     clock.tick(60)
-
-
-
-
-
-
-
-
 
 pygame.quit()
